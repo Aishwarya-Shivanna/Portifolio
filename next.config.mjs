3bd64b4305e67f0c2bdd8bc4934673dc35f9704d
Aishwarya Shivanna <as6766@nau.edu>
2025-12-18 22:39:23 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // 1) In Next.js 14+, this tells Next.js to produce static files in "out/" during `next build`.
-    output: 'export',
-
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'bjornmelin.io'
-            }
-        ]
-    },
-    trailingSlash: true,
-    reactStrictMode: true,
-
-    env: {
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-        NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL
-    }
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/Portifolio',
+  assetPrefix: '/Portifolio/',
 };
-
 export default nextConfig;
